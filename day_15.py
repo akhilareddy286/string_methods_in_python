444c0aef61e2fa9ef712830d65ed5cec4e6dbd28
akhilareddy286 <102401859+akhilareddy286@users.noreply.github.com>
2022-03-31 11:03:01 +0530

    Add files via upload

---
+++ day_15.py
@@ -0,0 +1,40 @@
+# n="akhila"
+# print(n.capitalize())
+
+# n="AKHILA"
+# print(n.casefold())
+# print(n.lower())
+print(n.isupper())
+
+# n="akHIla"
+# print(n.casefold())
+# print(n.lower())
+
+# n="akhila reddy"
+# print(n.upper())
+# print(n.islower())
+
+# n="reddy"
+# print(n.istitle())
+# print(n.title())
+
+# n="akhila"
+# print(n.center(20))
+# print(n.center(46,"@"))
+# print(n.center(4))
+# print(n.center(50,"$"))
+# print(n.center(30,"&"))
+
+# print(type(n))
+# print(n.encode())
+# a=n.encode()
+# print(type(a))
+# print(a.decode())
+
+# n=b"reddy"
+# print(type(n))
+# print(n.decode())
+# a=n.decode()
+# print(type(a))
+# print(a.encode())
+
